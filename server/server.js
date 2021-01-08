07710d55f588b0e9d76de6c226bca36475606727
vyash5075 <vyash5075@gmail.com>
2021-01-09 01:32:45 +0530

    paypal payment integrated

--- server/server.js
+++ server/server.js
@@ -29,6 +29,10 @@ app.use((req, res, next) => {
 app.use("/api/products/", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
 //global middleware url hi galat hai .   to handle 404 status code
 app.use(notFound);
 
